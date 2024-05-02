b95372b9d2501c799e23b231003b3ca9e3488804
OHLAntoine <antoine.ohl@gmail.com>
2024-05-02 15:53:55 +0200

    Mise en place de la présentation d'un champion

--- src/stores/index.js
+++ src/stores/index.js
@@ -16,8 +16,17 @@ export const useChampionStore = defineStore('champions', {
     },
     getChampion(name) {
       axiosClient.get(`champion/${name}.json`).then(({ data }) => {
-        this.champion = data.data
+        this.champion = data.data[name]
       })
+    },
+    getChampionImage(name) {
+      return `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg`
+    },
+    getSpellImage(id) {
+      return `https://ddragon.leagueoflegends.com/cdn/14.9.1/img/spell/${id}.png`
+    },
+    getPassiveImage(name) {
+      return `https://ddragon.leagueoflegends.com/cdn/14.9.1/img/passive/${name}`
     }
   }
 })
--- src/views/ChampionView.vue
+++ src/views/ChampionView.vue
@@ -7,8 +7,8 @@
         name="name"
         id="name"
         class="p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-        v-model="name"
-        @change="store.getChampion(name)"
+        v-model="champ"
+        @change="store.getChampion(champ)"
       >
         <option value="">Choisissez votre champion.</option>
         <option v-for="(item, index) in store.champions" :key="index" :value="index">
@@ -18,14 +18,79 @@
     </form>
   </section>
 
-  <pre>{{ store.champion }}</pre>
+  <section class="max-h-96 lg:px-24 px-6 flex gap-8" v-if="champ != ''">
+    <img :src="store.getChampionImage(champ)" alt="image" style="height: 100%; width: 20%" />
+    <div class="flex flex-col justify-start w-5/12">
+      <div class="flex items-center gap-2">
+        <h2 class="font-bold text-2xl">{{ store.champion.name }}</h2>
+        <span
+          v-for="(tag, index) in store.champion.tags"
+          :key="index"
+          class="bg-gray-300 rounded px-1 mb-1"
+          >{{ tag }}</span
+        >
+      </div>
+      <h4 class="font-semibold text-xl">{{ store.champion.title }}</h4>
+      <ul class="flex flex-col flex-grow justify-around items-start">
+        <li
+          v-for="(tip, index) in store.champion.enemytips"
+          :key="index"
+          class="flex justify-center"
+        >
+          <img
+            src="../assets/icons/icons8-erreur-50.png"
+            alt="warning icon"
+            style="height: 1rem; width: 1rem"
+            class="mt-1 me-4"
+          />
+          {{ tip }}
+        </li>
+        <li>
+          <h4 class="font-semibold text-xl">
+            {{ store.champion.passive.name }} <span class="font-normal">(passif)</span>
+          </h4>
+          <div class="flex items-center gap-2">
+            <img
+              :src="store.getPassiveImage(store.champion.passive.image.full)"
+              alt="Passive image"
+              style="height: 64px; width: 64px"
+            />
+            <p>{{ store.champion.passive.description.replace(/\<.+?\>/g, '') }}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="flex flex-col justify-start">
+      <h4 class="font-semibold text-xl mb-2">Sorts :</h4>
+      <ul class="flex flex-col flex-grow justify-around">
+        <li v-for="(spell, index) in store.champion.spells" :key="index">
+          <div class="flex items-center gap-4">
+            <img
+              :src="store.getSpellImage(spell.id)"
+              alt="Image du sort"
+              style="height: 64px; width: 64px"
+            />
+            <div class="flex flex-col">
+              <h5 class="font-semibold">{{ spell.name }}</h5>
+              <p>
+                Délai de récupération : <span class="font-medium">{{ spell.cooldownBurn }}</span>
+              </p>
+              <p>
+                Coût en mana : <span class="font-medium">{{ spell.costBurn }}</span>
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import { useChampionStore } from '@/stores'
 import { ref, onMounted } from 'vue'
 
-const name = ref('')
+const champ = ref('')
 
 const store = useChampionStore()
 onMounted(() => {
