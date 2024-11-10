0c54437489ff193fde432708ec78f4eb040adf45
OHLAntoine <antoine.ohl@gmail.com>
2024-11-10 17:18:51 +0100

    Patch 14.22 with production files

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://ddragon.leagueoflegends.com/cdn/14.20.1/data/fr_FR/'
+  baseURL: 'https://ddragon.leagueoflegends.com/cdn/14.22.1/data/fr_FR/'
 })
 
 export default axiosClient
