f8b7bd145601fd181eb844e6e2ac020d8ee5bdbd
OHLAntoine <antoine.ohl@gmail.com>
2025-03-17 17:16:18 +0100

    Patch 15.5.1

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://ddragon.leagueoflegends.com/cdn/15.3.1/data/fr_FR/'
+  baseURL: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/fr_FR/'
 })
 
 export default axiosClient
